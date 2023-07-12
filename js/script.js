040ebd1aba70ba7d2a6ea4d26c425ff29df5dec3
MrShifu01 <stander.christian.com>
2023-07-12 09:42:08 +0200

    a

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// Add custom JavaScript here
+function runTypingEffect () {
+    const text = "I am Christian Stander."
+    const typingElement = document.getElementById('typing-text')
+    const typingDelay = 100
+
+    typeText(text, typingElement, typingDelay)
+}
+
+function typeText (text, typingElement, typingDelay) {
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(() => {
+            typingElement.textContent += text[i]
+        }, typingDelay * i)
+    }
+}
+
+document.addEventListener('DOMContentLoaded', runTypingEffect)
